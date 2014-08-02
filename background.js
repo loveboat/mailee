67ffd3a22019d12980912f3cc901e1b8bddcafc6
David Lusby <lusbyd@gmail.com>
2014-08-02 10:17:17 +0200

    changed title and description to something a little less dull

---
+++ background.js
@@ -0,0 +1,47 @@
+chrome.tabs.onUpdated.addListener(function(tabId, props, tab) {
+  if (props.status === "complete") {
+    tabsCount();
+  }
+});
+
+chrome.tabs.onSelectionChanged.addListener(function(tabId, props) {
+  tabsCount();
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, props) {
+  tabsCount();
+});
+
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//   tabsCount();
+// });
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+	console.log('alarm triggered');
+
+	if (alarm.name === 'gmail-killer') {
+		chrome.tabs.query({}, function (tabs) {
+			tabs.forEach(function(tab) {
+				if (tab.url.indexOf('https://mail.google.com/mail/') !== -1) {
+					console.log('gmail tab closed');
+					chrome.tabs.remove(tab.id);
+				}
+			});
+		});
+	}
+});
+
+function tabsCount() {
+	chrome.tabs.query({}, function (tabs) {
+		chrome.browserAction.setBadgeText({text: tabs.length.toString()});
+
+		tabs.forEach(function (tab) {
+			if (tab.url.indexOf('https://mail.google.com/mail/') !== -1) {
+				chrome.browserAction.setBadgeText({text: 'Yes'});
+
+				console.log('setting alarm');
+				chrome.alarms.create('gmail-killer', {delayInMinutes: 0.1});
+			}
+		});
+	});
+}
