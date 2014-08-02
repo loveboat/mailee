86094dd1f1d6bc4e7f934113aa1e6855f10a315c
edenfull <spacecamper@gmail.com>
2014-08-02 11:48:09 +0200

    Improved popup.html.

--- popup.js
+++ popup.js
@@ -1,4 +1,6 @@
 window.onload = function() {
+  document.getElementById("currentSetting").innerHTML += chrome.extension.getBackgroundPage().timeToBeOpenInMinutes + " min";
+
   document.getElementById("button").onclick = function() {
 		var delay = document.getElementById("delay").value;
 		console.log('value set: ' + delay);
