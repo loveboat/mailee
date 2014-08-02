62739804a46dddfeee94564b258f2f0b2ee23981
David Lusby <lusbyd@gmail.com>
2014-08-02 12:02:53 +0200

    tidied popup - pre-fill the delay time in the input

--- popup.js
+++ popup.js
@@ -1,8 +1,9 @@
 window.onload = function() {
-  document.getElementById("currentSetting").innerHTML += chrome.extension.getBackgroundPage().timeToBeOpenInMinutes + " min";
-
+	var delay = chrome.extension.getBackgroundPage().timeToBeOpenInMinutes;
+	document.getElementById("delay").value = delay;
+  
   document.getElementById("button").onclick = function() {
-		var delay = document.getElementById("delay").value;
+		delay = document.getElementById("delay").value;
     chrome.extension.sendMessage({
       type: 'set-delay',
       message: delay
