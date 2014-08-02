34dc38b755ed7438c62008bd38acda8626c3b26b
David Lusby <lusbyd@gmail.com>
2014-08-02 11:52:21 +0200

    removed unneeded log message

--- popup.js
+++ popup.js
@@ -3,7 +3,6 @@ window.onload = function() {
 
   document.getElementById("button").onclick = function() {
 		var delay = document.getElementById("delay").value;
-		console.log('value set: ' + delay);
     chrome.extension.sendMessage({
       type: 'set-delay',
       message: delay
